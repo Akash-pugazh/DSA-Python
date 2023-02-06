3f5ab6d5cf851d25bc41433ff9613a451f85472d
ViRuS <aakashit13@gmail.com>
2023-02-07 00:03:34 +0530

    .

---
+++ Recursion/RecursiveEg2.py
@@ -0,0 +1,8 @@
+def RecursiveNumber(n):
+    if n < 1:
+        print(f"End of n : {n}")
+    else:
+        print(f"Progressing n : {n}")
+        RecursiveNumber(n - 1)
+        
+RecursiveNumber(10)
---
+++ Recursion/RussianDollEg.py
@@ -0,0 +1,8 @@
+def RussianDoll(dollCount):
+    if dollCount == 0:
+        print("All Dolls are Opened")
+    else:
+        print(f"Doll to be Opened : {dollCount}")
+        RussianDoll(dollCount - 1)
+
+RussianDoll(10)
