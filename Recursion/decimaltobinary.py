c2dc2986d94e2c35b4bfdaf335090c8babb4eb05
ViRuS <aakashit13@gmail.com>
2023-02-09 23:15:20 +0530

    .

--- Recursion/decimaltobinary.py
+++ Recursion/decimaltobinary.py
@@ -1,1 +1,16 @@
-def DecimalToBinary(n):
+
+class Solution:
+    def DecimalToBinary(self, n, result = ""):
+        assert 0 <= n == int(n), "N value should be a postive integer"
+        if n == 1:
+            return f"1{result}"
+        elif n == 0:
+            return 0
+        else:
+            result += str(n % 2)
+            return self.DecimalToBinary(int(n / 2), result)
+
+
+test1 = print(Solution().DecimalToBinary(16))
+test2 = print(Solution().DecimalToBinary(8))
+test3 = print(Solution().DecimalToBinary(1.3))
