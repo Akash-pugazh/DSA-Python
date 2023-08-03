d69db620eb23523a4f7f5e4e774a3d69ee382a4c
ViRuS <aakashit13@gmail.com>
2023-08-03 22:47:57 +0530

    abstraction

--- Oops/main.py
+++ Oops/main.py
@@ -1,7 +1,17 @@
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, pwd):
         self.name = name
-        print("Akash")
+        self.__pwd = pwd
+
+    def get_pwd(self):
+        return self.__pwd
+
+    def set_pwd(self, pwd):
+        self.__pwd = pwd
+
+    def get_details(self):
+        return {"name": self.name, "pass": self.get_pwd()}
 
 
-akash = Person("Akash")
+akash = Person("Akash", "1234")
+print(akash.get_details())
